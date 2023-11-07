4e8cb395789f6a8b476e6b34615c839d2404de35
TNguoi <44415704+lyervo@users.noreply.github.com>
2023-11-07 19:18:35 +0800

    I give up, set cors to all

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_URL,
+    origin: '*',
     allowedHeaders: ['sa-client'],
     credentials: true,
   },
